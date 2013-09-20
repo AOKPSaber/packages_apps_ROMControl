b8c3f8a82a355c26f12b712fa2a9602ff439aa17
0xD34D <clark@scheffsblend.com>
2013-09-21 00:10:56 +0200

    Add network stats to statusbar (2/2)
    PS3: Fixed translatable items and czech translation
    
    Change-Id: I14fb612bdbac46e8ae6f4155bd42fe10a0b0e8d9
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>
    
    Conflicts:
    	res/xml/prefs_statusbar_signal.xml
    	src/com/aokp/romcontrol/fragments/StatusBarSignal.java
    
    PS4: Rebased, code cleanup, added custom text color
    
    Change-Id: I064c8f77a0dba439278de2cc7324841307d58b84
    
    Conflicts:
    	res/values-cs/strings.xml
    	res/values/strings.xml
    	res/xml/prefs_statusbar_signal.xml
    	src/com/aokp/romcontrol/fragments/StatusBarSignal.java

--- src/com/aokp/romcontrol/fragments/StatusBarSignal.java
+++ src/com/aokp/romcontrol/fragments/StatusBarSignal.java
@@ -15,12 +15,18 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 public class StatusBarSignal extends AOKPPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_NETWORK_STATS = "status_bar_show_network_stats";
+    private static final String STATUS_BAR_NETWORK_STATS_UPDATE = "status_bar_network_status_update";
+
     ListPreference mDbmStyletyle;
     ListPreference mWifiStyle;
+    private ListPreference mNetStatsUpdate;
     ColorPickerPreference mColorPicker;
     ColorPickerPreference mWifiColorPicker;
+    private ColorPickerPreference mNetStatsColorPicker;
     CheckBoxPreference mHideSignal;
     CheckBoxPreference mAltSignal;
+    private CheckBoxPreference mNetworkStats;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,8 @@ public class StatusBarSignal extends AOKPPreferenceFragment implements
         setTitle(R.string.title_statusbar_signal);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_statusbar_signal);
+
+        PreferenceScreen prefs = getPreferenceScreen();
 
         mDbmStyletyle = (ListPreference) findPreference("signal_style");
         mDbmStyletyle.setOnPreferenceChangeListener(this);
@@ -42,8 +50,15 @@ public class StatusBarSignal extends AOKPPreferenceFragment implements
         mWifiStyle.setValue(Integer.toString(Settings.System.getInt(mContentRes,
                 Settings.System.STATUSBAR_WIFI_SIGNAL_TEXT, 0)));
 
+        mNetworkStats = (CheckBoxPreference) findPreference(STATUS_BAR_NETWORK_STATS);
+        mNetworkStats.setChecked((Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_NETWORK_STATS, 0) == 1));
+
         mWifiColorPicker = (ColorPickerPreference) findPreference("wifi_signal_color");
         mWifiColorPicker.setOnPreferenceChangeListener(this);
+
+        mNetStatsColorPicker = (ColorPickerPreference) findPreference("status_bar_network_status_color");
+        mNetStatsColorPicker.setOnPreferenceChangeListener(this);
 
         mHideSignal = (CheckBoxPreference) findPreference("hide_signal");
         mHideSignal.setChecked(Settings.System.getBoolean(mContentRes,
@@ -53,6 +68,13 @@ public class StatusBarSignal extends AOKPPreferenceFragment implements
         mAltSignal.setChecked(Settings.System.getBoolean(mContentRes,
                 Settings.System.STATUSBAR_SIGNAL_CLUSTER_ALT, false));
 
+        mNetStatsUpdate = (ListPreference) findPreference(STATUS_BAR_NETWORK_STATS_UPDATE);
+        long statsUpdate = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL, 500);
+        mNetStatsUpdate.setValue(String.valueOf(statsUpdate));
+        mNetStatsUpdate.setSummary(mNetStatsUpdate.getEntry());
+        mNetStatsUpdate.setOnPreferenceChangeListener(this);
+        
         if (Integer.parseInt(mDbmStyletyle.getValue()) == 0) {
             mColorPicker.setEnabled(false);
             mColorPicker.setSummary(R.string.enable_signal_text);
@@ -75,6 +97,11 @@ public class StatusBarSignal extends AOKPPreferenceFragment implements
         } else if (preference == mAltSignal) {
             Settings.System.putBoolean(mContentRes,
                     Settings.System.STATUSBAR_SIGNAL_CLUSTER_ALT, mAltSignal.isChecked());
+            return true;
+        } else if (preference == mNetworkStats) {
+            boolean value = mNetworkStats.isChecked();
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_NETWORK_STATS, value ? 1 : 0);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -125,6 +152,22 @@ public class StatusBarSignal extends AOKPPreferenceFragment implements
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(mContentRes,
                     Settings.System.STATUSBAR_WIFI_SIGNAL_TEXT_COLOR, intHex);
+            return true;
+        } else if (preference == mNetStatsUpdate) {
+            long updateInterval = Long.valueOf((String) newValue);
+            int index = mNetStatsUpdate.findIndexOfValue((String) newValue);
+            Settings.System.putLong(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL, updateInterval);
+            mNetStatsUpdate.setSummary(mNetStatsUpdate.getEntries()[index]);
+            return true;
+        } else if (preference == mNetStatsColorPicker) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hex);
+
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.STATUS_BAR_NETWORK_STATS_TEXT_COLOR, intHex);
             return true;
         }
         return false;
